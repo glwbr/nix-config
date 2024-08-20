2acb57d11f3776cde6eba36ead513a9c7557ffd1
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2024-08-20 10:58:59 -0300

    feat: add obs-studio

---
+++ home/software/utilities/obs-studio/default.nix
@@ -0,0 +1,17 @@
+{pkgs, ...}: {
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      droidcam-obs
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi
+      obs-vkcapture
+    ];
+
+    #home.file.".config/obs-studio" = {
+    #  source = ./config;
+    #};
+  };
+}
