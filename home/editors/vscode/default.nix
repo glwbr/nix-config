e29b89627fabb3a32ca7554a0af68fb1742f4929
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2024-09-03 00:10:44 -0300

    feat: add md-preview-enhanced

--- home/editors/vscode/default.nix
+++ home/editors/vscode/default.nix
@@ -7,15 +7,17 @@
       dbaeumer.vscode-eslint
       eamodio.gitlens
       ms-azuretools.vscode-docker
+      shd101wyy.markdown-preview-enhanced
       vscodevim.vim
       vue.volar
-      yzhang.markdown-all-in-one
     ];
 
     mutableExtensionsDir = false;
 
     userSettings = {
       "keyboard.dispatch" = "keyCode";
+      "git.openRepositoryInParentFolders" = "always";
+      "editor.minimap.enabled" = false;
     };
   };
 }
