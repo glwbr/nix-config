277741d21cfc2cb0bb3f990971908bd1412ba217
Glauber Santana <glauber.silva14@gmail.com>
2024-09-07 14:21:58 -0300

    feat: add libvirtd, qemu-kvm

---
+++ system/programs/virt.nix
@@ -0,0 +1,31 @@
+{pkgs, ...}: {
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [pkgs.OVMFFull.fd];
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    spice
+    spice-gtk
+    spice-protocol
+    virt-viewer
+  ];
+  programs.virt-manager.enable = true;
+
+  home-manager.users.glwbr = {
+    dconf.settings = {
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = ["qemu:///system"];
+        uris = ["qemu:///system"];
+      };
+    };
+  };
+}
