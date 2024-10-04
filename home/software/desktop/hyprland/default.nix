e5ff4459cf00e7b931eee406f7a412fb229dc02b
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2024-10-04 19:26:03 -0300

    feat: add hyprland

---
+++ home/software/desktop/hyprland/default.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  inputs,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    inputs.hyprland.homeManagerModules.default
+    ./settings.nix
+  ];
+
+  home.packages = with pkgs; [
+    grimblast
+    hyprpicker
+  ];
+
+  # Enable hyprland
+  wayland.windowManager.hyprland = {
+    enable = true;
+    systemd = {
+      variables = [ "--all" ];
+      extraCommands = lib.mkBefore [
+        "systemctl --user stop graphical-session.target"
+        "systemctl --user start hyprland-session.target"
+      ];
+    };
+  };
+}
---
+++ home/software/desktop/hyprland/settings.nix
@@ -0,0 +1,64 @@
+{ config, ... }:
+let
+  pointer = config.stylix.cursor;
+in
+{
+  imports = [ ./keybindings.nix ];
+  wayland.windowManager.hyprland.settings = {
+    exec = [
+      "hyprctl setcursor ${pointer.name} ${toString pointer.size}"
+    ];
+
+    animations = {
+      enabled = true;
+      bezier = [
+        "fluent_decel, 0, 0.2, 0.4, 1"
+        "easeOutCirc, 0, 0.55, 0.45, 1"
+        "easeOutCubic, 0.33, 1, 0.68, 1"
+        "easeinoutsine, 0.37, 0, 0.63, 1"
+      ];
+
+      animation = [
+        "windowsIn, 1, 1.7, easeOutCubic, slide"
+        "windowsOut, 1, 1.7, easeOutCubic, slide"
+        "windowsMove, 1, 2.5, easeinoutsine, slide"
+        "border, 1, 2, default"
+
+        # Fading
+        "fadeIn, 1, 3, easeOutCubic"
+        "fadeOut, 1, 3, easeOutCubic"
+        "fadeSwitch, 1, 5, easeOutCirc"
+        "fadeShadow, 1, 5, easeOutCirc"
+        "fadeDim, 1, 6, fluent_decel"
+        "border, 1, 2.7, easeOutCirc"
+        "workspaces, 1, 2, fluent_decel, slide"
+        "specialWorkspace, 1, 3, fluent_decel, slidevert"
+      ];
+    };
+
+    debug.disable_logs = false;
+
+    gestures = {
+      workspace_swipe = true;
+      workspace_swipe_forever = true;
+    };
+
+    input = {
+      kb_layout = "us,us";
+      kb_variant = ",intl";
+      kb_options = "caps:swapescape,grp:alt_space_toggle";
+
+      repeat_delay = 170;
+      repeat_rate = 30;
+    };
+
+    misc = {
+      disable_hyprland_logo = true;
+      disable_splash_rendering = true;
+      vfr = true;
+      vrr = true;
+    };
+
+    xwayland.force_zero_scaling = true;
+  };
+}
