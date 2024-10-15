e609eea2bded6a4cdaffef6583c119f36eb04a3a
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2024-10-15 02:01:17 -0300

    refactor: extended library

--- lib/default.nix
+++ lib/default.nix
@@ -1,25 +1,5 @@
 { lib, ... }:
-
-let
-  inherit (lib) mkOption types;
-in
-
-rec {
-  mkOpt =
-    type: default: description:
-    mkOption { inherit type default description; };
-
-  mkOpt' = type: default: mkOpt type default null;
-
-  mkBoolOpt = mkOpt types.bool;
-
-  mkBoolOpt' = mkOpt' types.bool;
-
-  enabled = {
-    enable = true;
-  };
-
-  disabled = {
-    enable = false;
-  };
+{
+  aria = import ./utils.nix { inherit lib; };
+  # helpers = import ./helpers.nix lib;
 }
---
+++ lib/utils.nix
@@ -0,0 +1,24 @@
+{ lib, ... }:
+
+let
+  inherit (lib) mkOption types;
+in
+rec {
+  mkOpt =
+    type: default: description:
+    mkOption { inherit type default description; };
+
+  mkOpt' = type: default: mkOpt type default null;
+
+  mkBoolOpt = mkOpt types.bool;
+
+  mkBoolOpt' = mkOpt' types.bool;
+
+  enabled = {
+    enable = true;
+  };
+
+  disabled = {
+    enable = false;
+  };
+}
