dcb9b5a87c24c9d1fb669df92aa690844eb6d2e3
Glauber Santana <glauber.silva14@gmail.com>
2024-10-19 02:16:03 -0300

    feat(tools): make direnv as module

--- home/software/terminal/tools/direnv/default.nix
+++ home/software/terminal/tools/direnv/default.nix
@@ -1,7 +1,28 @@
-_: {
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-    enableZshIntegration = true;
+{
+  config,
+  lib,
+  namespace,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (lib.aria) mkBoolOpt enabled;
+
+  cfg = config.aria.programs.terminal.tools.direnv;
+in
+{
+  options.aria.programs.terminal.tools.direnv = {
+    enable = mkBoolOpt false "Whether or not to enable direnv.";
+  };
+
+  config = mkIf cfg.enable {
+    home.sessionVariables.DIRENV_LOG_FORMAT = "";
+
+    programs.direnv = {
+      enable = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+      nix-direnv = enabled;
+    };
   };
 }
