f80c3a76b5960025bf9c46dc285886013241da15
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2024-10-20 13:01:06 -0300

    refactor(zsh): split suggestion,completions settings

--- home/software/terminal/shells/zsh/plugins/default.nix
+++ home/software/terminal/shells/zsh/plugins/default.nix
@@ -7,7 +7,7 @@ pkgs: {
       src = pkgs.fetchFromGitHub {
         owner = "Aloxaf";
         repo = "fzf-tab";
-        rev = "5a81e13792a1eed4a03d2083771ee6e5b616b9ab";
+        rev = "b6e1b22458a131f835c6fe65bdb88eb45093d2d2";
         sha256 = "0lfl4r44ci0wflfzlzzxncrb3frnwzghll8p365ypfl0n04bkxvl";
       };
     }
@@ -18,16 +18,6 @@ pkgs: {
         repo = "pure";
         rev = "92b8e9057988566b37ff695e70e2e9bbeb7196c8";
         sha256 = "1q6pxa1lq21f9956v1684bgkfrkdfx4jy9n8gvffp0672agapcsd";
-      };
-    }
-    {
-      name = "fast-syntax-highlighting";
-      file = "fast-syntax-highlighting.plugin.zsh";
-      src = pkgs.fetchFromGitHub {
-        owner = "zdharma-continuum";
-        repo = "fast-syntax-highlighting";
-        rev = "cf318e06a9b7c9f2219d78f41b46fa6e06011fd9";
-        sha256 = "1bmrb724vphw7y2gwn63rfssz3i8lp75ndjvlk5ns1g35ijzsma5";
       };
     }
     {
