1f6f603e70974d0615a876238522ac860b0a5082
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2024-10-25 18:40:42 -0300

    feat(hosts): add emulatedSystem support to aarch64

--- hosts/sonata/boot.nix
+++ hosts/sonata/boot.nix
@@ -6,6 +6,8 @@
       kernelModules = [ "nfs" ];
     };
 
+    binfmt.emulatedSystems = [ "aarch64-linux" ];
+
     kernelModules = [ "v4l2loopback" ];
     kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
     extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
