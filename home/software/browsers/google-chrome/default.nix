c30d22523147d44171fab2b06c5c0f0b2892c7db
Glauber Santana <glauber.silva14@gmail.com>
2024-10-28 12:40:44 -0300

    chore: remove chromium, librewolf, add google-chrome

---
+++ home/software/browsers/google-chrome/default.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.aria.programs.browsers.google-chrome;
+
+  inherit (lib.aria) mkBoolOpt;
+in
+{
+  options.aria.programs.browsers.google-chrome = {
+    enable = mkBoolOpt false "Whether or not to enable Google Chrome.";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [ pkgs.google-chrome ];
+  };
+}
