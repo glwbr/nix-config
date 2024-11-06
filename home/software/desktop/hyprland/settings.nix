6aed405b280884e7c7beb8e7abe82f287a644854
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2024-11-06 13:06:18 -0300

    feat(hyprland): add extra keybindings, windowrules and compositor settings

--- home/software/desktop/hyprland/settings.nix
+++ home/software/desktop/hyprland/settings.nix
@@ -3,7 +3,10 @@ let
   pointer = config.stylix.cursor;
 in
 {
-  imports = [ ./keybindings.nix ];
+  imports = [
+    ./keybindings.nix
+    ./windowrules.nix
+  ];
   wayland.windowManager.hyprland.settings = {
     exec = [ "hyprctl setcursor ${pointer.name} ${toString pointer.size}" ];
 
@@ -36,7 +39,51 @@ in
       ];
     };
 
+    decoration = {
+      rounding = 4;
+
+      blur = {
+        enabled = true;
+        brightness = 1.0;
+        contrast = 1.0;
+        noise = 2.0e-2;
+        new_optimizations = true;
+
+        passes = 3;
+        size = 8;
+        xray = true;
+      };
+
+      drop_shadow = false;
+      shadow_ignore_window = true;
+      shadow_offset = "0 2";
+      shadow_range = 20;
+      shadow_render_power = 3;
+    };
+
     debug.disable_logs = false;
+
+    dwindle = {
+      no_gaps_when_only = false;
+      pseudotile = true;
+      preserve_split = true;
+      smart_split = false;
+      smart_resizing = false;
+    };
+
+    general = {
+      gaps_in = 2;
+      gaps_out = 4;
+      border_size = 1;
+
+      monitor = [
+        "eDP-1, 1920x1080@60, 0x0, 1"
+        "HDMI-A-1, 2560x1440@60, 1920x0, 1"
+      ];
+
+      allow_tearing = true;
+      resize_on_border = true;
+    };
 
     gestures = {
       workspace_swipe = true;
@@ -44,19 +91,33 @@ in
     };
 
     input = {
+      accel_profile = "flat";
+      follow_mouse = true;
+
       kb_layout = "us,us";
       kb_variant = ",intl";
       kb_options = "caps:swapescape,grp:alt_space_toggle";
 
       repeat_delay = 170;
       repeat_rate = 30;
+
+      touchpad = {
+        disable_while_typing = true;
+        scroll_factor = 0.1;
+      };
     };
 
     misc = {
       disable_hyprland_logo = true;
       disable_splash_rendering = true;
+
+      force_default_wallpaper = false;
+
+      enable_swallow = true;
+      swallow_regex = "^(A|a)lacritty|footclient|foot$";
+
       vfr = true;
-      vrr = true;
+      vrr = 2;
     };
 
     xwayland.force_zero_scaling = true;
