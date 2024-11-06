8c261cae4cc1a5d72bcd3d75d2abc52dd989c2e4
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2024-11-05 23:32:35 -0300

    feat: i3, wireless and hyprland into modules

--- hosts/shared/optional/i3/default.nix
+++ hosts/shared/optional/i3/default.nix
@@ -1,8 +1,19 @@
-_: {
-  services.xserver = {
-    enable = true;
-    desktopManager.xterm.enable = false;
+{ config, lib, ... }:
+let
+  cfg = config.aria.wms.i3;
+  inherit (lib.aria) mkBoolOpt;
+in
+{
+  options.aria.wms.i3 = {
+    enable = mkBoolOpt false "Whether or not to enable i3.";
+  };
 
-    windowManager.i3.enable = true;
+  config = lib.mkIf cfg.enable {
+    services.xserver = {
+      enable = true;
+      desktopManager.xterm.enable = false;
+
+      windowManager.i3.enable = true;
+    };
   };
 }
--- hosts/shared/optional/wireless/default.nix
+++ hosts/shared/optional/wireless/default.nix
@@ -1,54 +1,64 @@
-{ config, ... }:
+{ config, lib, ... }:
+let
+  cfg = config.aria.wireless.supplicant;
+  inherit (lib.aria) mkBoolOpt;
+in
 {
-  sops.secrets.wireless = {
-    sopsFile = ../../secrets.yaml;
-    neededForUsers = true;
+  options.aria.wireless.supplicant = {
+    enable = mkBoolOpt false "Whether or not to enable wireless support through wpa_supplicant.";
   };
 
-  networking.wireless = {
-    enable = true;
-    fallbackToWPA2 = false;
-
-    secretsFile = config.sops.secrets.wireless.path;
-
-    networks = {
-      "UAIFAI" = {
-        pskRaw = "ext:uaifai_24g";
-      };
-
-      "UAIFAI_4G" = {
-        pskRaw = "ext:uaifai_mobile";
-      };
-
-      "UAIFAI_5G" = {
-        pskRaw = "ext:uaifai_5g";
-        priority = 99;
-      };
-
-      "SALA DOS FUNDOS_5G" = {
-        pskRaw = "ext:sala_c4w";
-        priority = 98;
-      };
-
-      "Club4Work" = {
-        pskRaw = "ext:c4w";
-      };
+  config = lib.mkIf cfg.enable {
+    sops.secrets.wireless = {
+      sopsFile = ../../secrets.yaml;
+      neededForUsers = true;
     };
 
-    # Imperative
-    allowAuxiliaryImperativeNetworks = true;
-    userControlled = {
+    networking.wireless = {
       enable = true;
-      group = "network";
+      fallbackToWPA2 = false;
+
+      secretsFile = config.sops.secrets.wireless.path;
+
+      networks = {
+        "UAIFAI" = {
+          pskRaw = "ext:uaifai_24g";
+        };
+
+        "UAIFAI_4G" = {
+          pskRaw = "ext:uaifai_mobile";
+        };
+
+        "UAIFAI_5G" = {
+          pskRaw = "ext:uaifai_5g";
+          priority = 99;
+        };
+
+        "SALA DOS FUNDOS_5G" = {
+          pskRaw = "ext:sala_c4w";
+          priority = 98;
+        };
+
+        "Club4Work" = {
+          pskRaw = "ext:c4w";
+        };
+      };
+
+      # Imperative
+      allowAuxiliaryImperativeNetworks = true;
+      userControlled = {
+        enable = true;
+        group = "network";
+      };
+
+      extraConfig = ''
+        update_config=1
+      '';
     };
 
-    extraConfig = ''
-      update_config=1
-    '';
+    # Ensure group exists
+    aria.user.extraGroups = [ "network" ];
+    systemd.services.wpa_supplicant.preStart = "touch /etc/wpa_supplicant.conf";
+    users.groups.network = { };
   };
-
-  # Ensure group exists
-  aria.user.extraGroups = [ "network" ];
-  systemd.services.wpa_supplicant.preStart = "touch /etc/wpa_supplicant.conf";
-  users.groups.network = { };
 }
