d96627c8428e2c0b2e0811831bda7411d64a1912
Glauber Santana <glauber.silva14@gmail.com>
2024-11-07 18:22:37 -0300

    feat(browsers): firefox as a module

--- home/software/browsers/firefox/default.nix
+++ home/software/browsers/firefox/default.nix
@@ -1,143 +1,230 @@
 {
-  pkgs,
+  config,
   lib,
+  pkgs,
   ...
 }:
+let
+  cfg = config.aria.programs.browsers.firefox;
+
+  inherit (lib.aria) mkBoolOpt mkOpt;
+in
 {
-  programs.browserpass.enable = true;
-  programs.firefox = {
-    enable = true;
-    profiles.glwbr = {
-      search = {
-        force = true;
-        default = "DuckDuckGo";
-        privateDefault = "DuckDuckGo";
-        order = [
-          "DuckDuckGo"
-          "Google"
-        ];
-      };
-      bookmarks = { };
-      extensions = with pkgs.inputs.firefox-addons; [
-        browserpass
-        bitwarden
-        # languagetool
-        ublock-origin
-      ];
-      bookmarks = { };
-      settings = {
-        "browser.startup.homepage" = "about:home";
 
-        # Disable irritating first-run stuff
-        "browser.disableResetPrompt" = true;
-        "browser.download.panel.shown" = true;
-        "browser.feeds.showFirstRunUI" = false;
-        "browser.messaging-system.whatsNewPanel.enabled" = false;
-        "browser.rights.3.shown" = true;
-        "browser.shell.checkDefaultBrowser" = false;
-        "browser.shell.defaultBrowserCheckCount" = 1;
-        "browser.startup.homepage_override.mstone" = "ignore";
-        "browser.uitour.enabled" = false;
-        "startup.homepage_override_url" = "";
-        "trailhead.firstrun.didSeeAboutWelcome" = true;
-        "browser.bookmarks.restore_default_bookmarks" = false;
-        "browser.bookmarks.addedImportButton" = true;
+  options.aria.programs.browsers.firefox = with lib; {
+    enable = mkBoolOpt false "Whether or not to enable firefox.";
+    gpuAcceleration = mkBoolOpt false "Enable GPU acceleration.";
+    hardwareDecoding = mkBoolOpt false "Enable hardware video decoding.";
+    settings = mkOpt types.attrs { } "Settings to apply to the profile.";
+  };
 
-        # Don't ask for download dir
-        "browser.download.useDownloadDir" = false;
+  config = lib.mkIf cfg.enable {
+    programs.browserpass.enable = true;
 
-        # Disable crappy home activity stream page
-        "browser.newtabpage.activity-stream.feeds.topsites" = false;
-        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-        "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts" = false;
-        "browser.newtabpage.blocked" = lib.genAttrs [
-          # Youtube
-          "26UbzFJ7qT9/4DhodHKA1Q=="
-          # Facebook
-          "4gPpjkxgZzXPVtuEoAL9Ig=="
-          # Wikipedia
-          "eV8/WsSLxHadrTL1gAxhug=="
-          # Reddit
-          "gLv0ja2RYVgxKdp0I5qwvA=="
-          # Amazon
-          "K00ILysCaEq8+bEqV/3nuw=="
-          # Twitter
-          "T9nJot5PurhJSy8n038xGA=="
-        ] (_: 1);
+    programs.firefox = {
+      enable = true;
 
-        # Disable some telemetry
-        "app.shield.optoutstudies.enabled" = false;
-        "browser.discovery.enabled" = false;
-        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
-        "browser.newtabpage.activity-stream.telemetry" = false;
-        "browser.ping-centre.telemetry" = false;
-        "datareporting.healthreport.service.enabled" = false;
-        "datareporting.healthreport.uploadEnabled" = false;
-        "datareporting.policy.dataSubmissionEnabled" = false;
-        "datareporting.sessions.current.clean" = true;
-        "devtools.onboarding.telemetry.logged" = false;
-        "toolkit.telemetry.archive.enabled" = false;
-        "toolkit.telemetry.bhrPing.enabled" = false;
-        "toolkit.telemetry.enabled" = false;
-        "toolkit.telemetry.firstShutdownPing.enabled" = false;
-        "toolkit.telemetry.hybridContent.enabled" = false;
-        "toolkit.telemetry.newProfilePing.enabled" = false;
-        "toolkit.telemetry.prompted" = 2;
-        "toolkit.telemetry.rejected" = true;
-        "toolkit.telemetry.reportingpolicy.firstRun" = false;
-        "toolkit.telemetry.server" = "";
-        "toolkit.telemetry.shutdownPingSender.enabled" = false;
-        "toolkit.telemetry.unified" = false;
-        "toolkit.telemetry.unifiedIsOptIn" = false;
-        "toolkit.telemetry.updatePing.enabled" = false;
-
-        # Disable fx accounts
-        "identity.fxaccounts.enabled" = false;
-        # Disable "save password" prompt
-        "signon.rememberSignons" = false;
-        # Harden
-        "privacy.trackingprotection.enabled" = true;
-        "dom.security.https_only_mode" = true;
-        # Layout
-        "browser.uiCustomization.state" = builtins.toJSON {
-          currentVersion = 20;
-          newElementCount = 5;
-          dirtyAreaCache = [
-            "nav-bar"
-            "PersonalToolbar"
-            "toolbar-menubar"
-            "TabsToolbar"
-            "widget-overflow-fixed-list"
+      profiles.glwbr = {
+        search = {
+          default = "DuckDuckGo";
+          force = true;
+          order = [
+            "DuckDuckGo"
+            "Google"
           ];
-          placements = {
-            PersonalToolbar = [ "personal-bookmarks" ];
-            TabsToolbar = [
-              "tabbrowser-tabs"
-              "new-tab-button"
-              "alltabs-button"
-            ];
-            nav-bar = [
-              "back-button"
-              "forward-button"
-              "stop-reload-button"
-              "urlbar-container"
-              "downloads-button"
-              "ublock0_raymondhill_net-browser-action"
-              "_testpilot-containers-browser-action"
-              "reset-pbm-toolbar-button"
-              "unified-extensions-button"
-            ];
-            toolbar-menubar = [ "menubar-items" ];
-            unified-extensions-area = [ ];
-            widget-overflow-fixed-list = [ ];
+          privateDefault = "DuckDuckGo";
+
+          engines = {
+            "Nix Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "type";
+                      value = "packages";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@np" ];
+            };
+
+            "NixOs Options" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/options";
+                  params = [
+                    {
+                      name = "channel";
+                      value = "unstable";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@no" ];
+            };
+
+            "NixOS Wiki" = {
+              urls = [ { template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; } ];
+              iconUpdateURL = "https://wiki.nixos.org/favicon.png";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = [ "@nw" ];
+            };
           };
-          seen = [
-            "save-to-pocket-button"
-            "developer-button"
-            "ublock0_raymondhill_net-browser-action"
-            "_testpilot-containers-browser-action"
-          ];
         };
+
+        bookmarks = { };
+
+        extensions = with pkgs.inputs.firefox-addons; [
+          angular-devtools
+          bitwarden
+          browserpass
+          darkreader
+          # languagetool
+          ublock-origin
+        ];
+
+        settings = lib.mkMerge [
+	  cfg.settings
+          {
+            "browser.startup.homepage" = "about:home";
+
+            # Disable irritating first-run stuff
+            "browser.disableResetPrompt" = true;
+            "browser.download.panel.shown" = true;
+            "browser.feeds.showFirstRunUI" = false;
+            "browser.messaging-system.whatsNewPanel.enabled" = false;
+            "browser.rights.3.shown" = true;
+            "browser.shell.checkDefaultBrowser" = false;
+            "browser.shell.defaultBrowserCheckCount" = 1;
+            "browser.startup.homepage_override.mstone" = "ignore";
+            "browser.uitour.enabled" = false;
+            "startup.homepage_override_url" = "";
+            "trailhead.firstrun.didSeeAboutWelcome" = true;
+            "browser.bookmarks.restore_default_bookmarks" = false;
+            "browser.bookmarks.addedImportButton" = true;
+
+            # Don't ask for download dir
+            "browser.download.useDownloadDir" = false;
+
+            # Disable crappy home activity stream page
+            "browser.newtabpage.activity-stream.feeds.topsites" = false;
+            "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+            "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts" = false;
+            "browser.newtabpage.blocked" = lib.genAttrs [
+              # Youtube
+              "26UbzFJ7qT9/4DhodHKA1Q=="
+              # Facebook
+              "4gPpjkxgZzXPVtuEoAL9Ig=="
+              # Wikipedia
+              "eV8/WsSLxHadrTL1gAxhug=="
+              # Reddit
+              "gLv0ja2RYVgxKdp0I5qwvA=="
+              # Amazon
+              "K00ILysCaEq8+bEqV/3nuw=="
+              # Twitter
+              "T9nJot5PurhJSy8n038xGA=="
+            ] (_: 1);
+
+            # Disable some telemetry
+            "app.shield.optoutstudies.enabled" = false;
+            "browser.discovery.enabled" = false;
+            "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+            "browser.newtabpage.activity-stream.telemetry" = false;
+            "browser.ping-centre.telemetry" = false;
+            "datareporting.healthreport.service.enabled" = false;
+            "datareporting.healthreport.uploadEnabled" = false;
+            "datareporting.policy.dataSubmissionEnabled" = false;
+            "datareporting.sessions.current.clean" = true;
+            "devtools.onboarding.telemetry.logged" = false;
+            "toolkit.telemetry.archive.enabled" = false;
+            "toolkit.telemetry.bhrPing.enabled" = false;
+            "toolkit.telemetry.enabled" = false;
+            "toolkit.telemetry.firstShutdownPing.enabled" = false;
+            "toolkit.telemetry.hybridContent.enabled" = false;
+            "toolkit.telemetry.newProfilePing.enabled" = false;
+            "toolkit.telemetry.prompted" = 2;
+            "toolkit.telemetry.rejected" = true;
+            "toolkit.telemetry.reportingpolicy.firstRun" = false;
+            "toolkit.telemetry.server" = "";
+            "toolkit.telemetry.shutdownPingSender.enabled" = false;
+            "toolkit.telemetry.unified" = false;
+            "toolkit.telemetry.unifiedIsOptIn" = false;
+            "toolkit.telemetry.updatePing.enabled" = false;
+
+            # Disable fx accounts
+            "identity.fxaccounts.enabled" = false;
+            # Disable "save password" prompt
+            "signon.rememberSignons" = false;
+            # Harden
+            "privacy.trackingprotection.enabled" = true;
+            "dom.security.https_only_mode" = true;
+            # Layout
+            "browser.uiCustomization.state" = builtins.toJSON {
+              currentVersion = 20;
+              newElementCount = 5;
+              dirtyAreaCache = [
+                "nav-bar"
+                "PersonalToolbar"
+                "toolbar-menubar"
+                "TabsToolbar"
+                "widget-overflow-fixed-list"
+              ];
+              placements = {
+                PersonalToolbar = [ "personal-bookmarks" ];
+                TabsToolbar = [
+                  "tabbrowser-tabs"
+                  "new-tab-button"
+                  "alltabs-button"
+                ];
+                nav-bar = [
+                  "back-button"
+                  "forward-button"
+                  "stop-reload-button"
+                  "urlbar-container"
+                  "downloads-button"
+                  "ublock0_raymondhill_net-browser-action"
+                  "_testpilot-containers-browser-action"
+                  "reset-pbm-toolbar-button"
+                  "unified-extensions-button"
+                ];
+                toolbar-menubar = [ "menubar-items" ];
+                unified-extensions-area = [ ];
+                widget-overflow-fixed-list = [ ];
+              };
+
+              seen = [
+                "save-to-pocket-button"
+                "developer-button"
+                "ublock0_raymondhill_net-browser-action"
+                "_testpilot-containers-browser-action"
+              ];
+            };
+          }
+          (lib.optionalAttrs cfg.gpuAcceleration {
+            "dom.webgpu.enabled" = true;
+            "gfx.webrender.all" = true;
+            "layers.gpu-process.enabled" = true;
+            "layers.mlgpu.enabled" = true;
+          })
+          (lib.optionalAttrs cfg.hardwareDecoding {
+            "media.ffmpeg.vaapi.enabled" = true;
+            "media.gpu-process-decoder" = true;
+            "media.hardware-video-decoding.enabled" = true;
+          })
+
+        ];
       };
     };
   };
