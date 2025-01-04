ccebf5f0015d26c8fe8127bd645bc57f50c91b99
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2025-01-03 21:59:28 -0300

    fix(locale/xkb): options and remove unneeded comments

--- modules/system/locale.nix
+++ modules/system/locale.nix
@@ -29,12 +29,5 @@ in {
 
     time.timeZone = lib.mkDefault "America/Bahia";
     time.hardwareClockInLocalTime = lib.mkDefault true;
-
-    # console.keyMap = lib.mkForce "us";
-    # services.xserver.xkb = {
-    #   layout = "us,us";
-    #   variant = ",intl";
-    #   options = "caps:swapescape,grp:alt_space_toggle";
-    # };
   };
 }
