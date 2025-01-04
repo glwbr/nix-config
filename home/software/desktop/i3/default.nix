c50df7e213de1cb44eb3560bf3cf4b6cbc02eb2f
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2025-01-03 21:57:34 -0300

    feat(i3): use default config file format

--- home/software/desktop/i3/default.nix
+++ home/software/desktop/i3/default.nix
@@ -1,13 +1,22 @@
-{pkgs, ...}: {
-  xsession.windowManager.i3 = {
-    enable = true;
-    package = pkgs.i3-gaps;
-    config = {
-      modifier = "Mod4";
-      gaps = {
-        inner = 2;
-        outer = 0;
-      };
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.aria.programs.wms.i3;
+  inherit (lib.aria) mkBoolOpt;
+in {
+  options.aria.programs.wms.i3 = {
+    enable = mkBoolOpt false "Whether to manager i3 settings";
+  };
+
+  config = lib.mkIf cfg.enable {
+    xsession.windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-gaps;
+      config = null;
+      extraConfig = builtins.readFile ./config;
     };
   };
 }
