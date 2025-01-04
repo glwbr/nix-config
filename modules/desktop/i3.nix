fbe42e169d49e82fdb080b1984775d3042e7f717
Glauber Santana <glauber.silva14@gmail.com>
2025-01-03 21:57:34 -0300

    feat(i3): use default config file format

--- modules/desktop/i3.nix
+++ modules/desktop/i3.nix
@@ -1,21 +1,24 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   cfg = config.aria.wms.i3;
   inherit (lib.aria) mkBoolOpt;
 in {
   options.aria.wms.i3 = {
-    enable = mkBoolOpt false "Whether or not to enable i3.";
+    enable = mkBoolOpt false "Whether to enable i3";
   };
 
   config = lib.mkIf cfg.enable {
     services.xserver = {
       enable = true;
       desktopManager.xterm.enable = false;
-
-      windowManager.i3.enable = true;
+      windowManager.i3 = {
+        enable = true;
+        package = pkgs.i3-gaps;
+      };
     };
   };
 }
