dd5f52946fbfb6a501214808d4a35d3e87c23c20
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2025-02-10 11:13:15 -0300

    feat: modules input

--- flake.nix
+++ flake.nix
@@ -37,85 +37,56 @@
     yazi.url = "github:sxyazi/yazi";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      disko,
-      hm,
-      ...
-    }@inputs:
+  outputs = { self, nixpkgs, disko, hm, ... }@inputs:
     let
       inherit (self) outputs;
-      lib = nixpkgs.lib.extend (final: _: import ./lib { lib = final; } // hm.lib);
+      lib =
+        nixpkgs.lib.extend (final: _: import ./lib { lib = final; } // hm.lib);
 
-      systems = [
-        "aarch64-linux"
-        "x86_64-linux"
-      ];
+      systems = [ "aarch64-linux" "x86_64-linux" ];
 
       forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
-      pkgsFor = lib.genAttrs systems (
-        system:
+      pkgsFor = lib.genAttrs systems (system:
         import nixpkgs {
           inherit system;
           config.allowUnfree = true;
-        }
-      );
-    in
-    {
+        });
+    in {
       inherit lib;
 
-      # homeManagerModules = import ./modules/home;
-      # nixosModules = import ./modules/nixos;
       overlays = import ./overlays { inherit inputs outputs; };
 
       devShells = forEachSystem (pkgs: import ./shell.nix { inherit pkgs; });
-      formatter = forEachSystem (pkgs: pkgs.nixfmt-rfc-style);
+      formatter = forEachSystem (pkgs: pkgs.alejandra);
       packages = forEachSystem (pkgs: import ./pkgs { inherit pkgs; });
 
       # 'nixos-rebuild --flake .#hostname'
       nixosConfigurations = {
         sonata = lib.nixosSystem {
-          modules = [
-            ./hosts/sonata
-            disko.nixosModules.default
-          ];
-          specialArgs = {
-            inherit inputs outputs;
-          };
+          modules = [ ./hosts/sonata ./modules disko.nixosModules.default ];
+          specialArgs = { inherit inputs outputs; };
         };
 
         sinfonia = lib.nixosSystem {
           system = "aarch64-linux";
-          modules = [
-            ./hosts/sinfonia
-            disko.nixosModules.default
-          ];
-          specialArgs = {
-            inherit inputs outputs;
-          };
+          modules = [ ./hosts/sinfonia disko.nixosModules.default ];
+          specialArgs = { inherit inputs outputs; };
         };
       };
 
       # 'home-manager --flake .#username@hostname'
       homeConfigurations = {
         "glwbr@sonata" = lib.homeManagerConfiguration {
-          extraSpecialArgs = {
-            inherit inputs outputs lib;
-          };
+          extraSpecialArgs = { inherit inputs outputs lib; };
           modules = [ ./home/glwbr/sonata.nix ];
           pkgs = pkgsFor.x86_64-linux;
         };
 
         "glwbr@sinfonia" = lib.homeManagerConfiguration {
-          extraSpecialArgs = {
-            inherit inputs outputs lib;
-          };
+          extraSpecialArgs = { inherit inputs outputs lib; };
           modules = [ ./home/glwbr/sinfonia.nix ];
           pkgs = pkgsFor.aarch64-linux;
         };
-
       };
     };
 }
