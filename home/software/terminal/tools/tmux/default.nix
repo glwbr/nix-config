9576e87d093df6aa25e808191656fe7a0fa92a8d
Glauber Santana <glauber.silva14@gmail.com>
2025-02-12 16:58:53 -0300

    refactor(tmux): move extraConfig to tmux.conf

--- home/software/terminal/tools/tmux/default.nix
+++ home/software/terminal/tools/tmux/default.nix
@@ -1,38 +1,12 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: let
+{ config, lib, pkgs, ... }:
+let
   inherit (lib) mkIf;
   inherit (lib.aria) mkBoolOpt;
 
   cfg = config.aria.programs.terminal.tools.tmux;
   plugins = with pkgs.tmuxPlugins; [
-    {
-      plugin = better-mouse-mode;
-      extraConfig =
-        # tmux
-        ''
-          set-option -g mouse on
-        '';
-    }
-    {
-      plugin = vim-tmux-navigator;
-    }
-    {
-      plugin = rose-pine;
-      extraConfig =
-        # tmux
-        ''
-          set -g @rose_pine_variant 'main'
-          set -g @rose_pine_host 'on'
-          set -g @rose_pine_date_time ""
-          set -g @rose_pine_user 'on'
-          set -g @rose_pine_left_separator '  '
-          set -g @rose_pine_bar_bg_disable 'on'
-        '';
-    }
+    { plugin = better-mouse-mode; }
+    { plugin = vim-tmux-navigator; }
     {
       plugin = resurrect;
       extraConfig =
@@ -40,8 +14,8 @@
         ''
           set -g @resurrect-strategy-nvim 'session'
           set -g @resurect-capture-pane-contents 'on'
-          set -g @resurect-processes 'ssh lazygit yazi'
-          set -g @resurect-dir '~/.tmux/resurrect'
+          set -g @resurect-processes 'ssh btm yazi'
+          set -g @resurect-dir '~/.local/state/tmux/resurrect'
         '';
     }
     {
@@ -65,29 +39,17 @@ in {
       baseIndex = 1;
       clock24 = true;
       disableConfirmationPrompt = true;
+      escapeTime = 0;
+      focusEvents = true;
+      keyMode = "vi";
       historyLimit = 100000;
       mouse = true;
+      newSession = true;
       prefix = "C-a";
       shell = "${pkgs.zsh}/bin/zsh";
-      terminal = "xterm-256color";
-      extraConfig =
-        # tmux
-        ''
-          bind -n M-H previous-window
-          bind -n M-L next-window
-          bind c new-window -c "#{pane_current_path}"
-          bind '-' split-window -v -c "#{pane_current_path}"
-          bind '|' split-window -h -c "#{pane_current_path}"
-          unbind %
-          unbind-key '"'
-          set -g default-terminal "tmux-256color"
-          set -sg escape-time 0
-          set -g renumber-windows on
-          set -g status-interval 3
-          set -g terminal-overrides "alacritty:RGB"
-          set -g status-position top
-        '';
+      terminal = "alacritty";
       inherit plugins;
+      extraConfig = builtins.readFile ./tmux.conf;
     };
   };
 }
