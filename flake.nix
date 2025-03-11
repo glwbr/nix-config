eeb445e15357dce1b10cdfee3b5cf01799845231
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2025-03-11 15:58:55 -0300

    refactor(flake): simplify system configuration and replace `nvim` input path
    - changed `nvim` input from GitHub to a local path.
    - refactored `outputs` to use `utils.mkSystem` for `nixosConfigurations`.
    - introduced `utils` from `./lib/utils.nix` for better reuse.

--- flake.nix
+++ flake.nix
@@ -6,86 +6,34 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    hm = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-
     hardware.url = "github:nixos/nixos-hardware";
-
-    impermanence.url = "github:nix-community/impermanence";
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     nvim.url = "github:glwbr/nvim";
-
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    stylix.url = "github:danth/stylix";
-
-    yazi.url = "github:sxyazi/yazi";
   };
 
-  outputs = { self, nixpkgs, disko, hm, ... }@inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      disko,
+      ...
+    }@inputs:
     let
       inherit (self) outputs;
-      lib =
-        nixpkgs.lib.extend (final: _: import ./lib { lib = final; } // hm.lib);
 
-      systems = [ "aarch64-linux" "x86_64-linux" ];
-
-      forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
-      pkgsFor = lib.genAttrs systems (system:
-        import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        });
-    in {
-      inherit lib;
-
+      lib = nixpkgs.lib.extend (final: prev: { aria = import ./lib { lib = final; }; });
+      utils = import ./lib/utils.nix { inherit lib inputs outputs; };
+    in
+    {
       overlays = import ./overlays { inherit inputs outputs; };
-
-      devShells = forEachSystem (pkgs: import ./shell.nix { inherit pkgs; });
-      formatter = forEachSystem (pkgs: pkgs.alejandra);
-      packages = forEachSystem (pkgs: import ./pkgs { inherit pkgs; });
+      devShells = utils.forEachSystem (pkgs: import ./shell.nix { inherit pkgs; });
 
       # 'nixos-rebuild --flake .#hostname'
       nixosConfigurations = {
-        sonata = lib.nixosSystem {
-          modules = [ ./hosts/sonata ./modules disko.nixosModules.default ];
-          specialArgs = { inherit inputs outputs; };
-        };
-
-        sinfonia = lib.nixosSystem {
+        sonata = utils.mkSystem { hostname = "sonata"; };
+        sinfonia = utils.mkSystem {
           system = "aarch64-linux";
-          modules = [ ./hosts/sinfonia disko.nixosModules.default ];
-          specialArgs = { inherit inputs outputs; };
-        };
-      };
-
-      # 'home-manager --flake .#username@hostname'
-      homeConfigurations = {
-        "glwbr@sonata" = lib.homeManagerConfiguration {
-          extraSpecialArgs = { inherit inputs outputs lib; };
-          modules = [ ./home/glwbr/sonata.nix ];
-          pkgs = pkgsFor.x86_64-linux;
-        };
-
-        "glwbr@sinfonia" = lib.homeManagerConfiguration {
-          extraSpecialArgs = { inherit inputs outputs lib; };
-          modules = [ ./home/glwbr/sinfonia.nix ];
-          pkgs = pkgsFor.aarch64-linux;
+          hostname = "sinfonia";
         };
       };
     };
