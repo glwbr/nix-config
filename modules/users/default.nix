11c8f2c742cd12e001da6366909ac5d491bf9703
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2025-03-11 14:21:28 -0300

    refactor(users): remove commented-out root user configuration

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -3,10 +3,10 @@
   lib,
   pkgs,
   ...
-}: let
+}:
+let
   inherit (lib) types;
   inherit (lib.aria) mkBoolOpt mkOpt;
-
   cfg = config.aria.users;
   ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
 
@@ -16,15 +16,16 @@
       email = mkOpt str null "The email of the user";
       fullName = mkOpt str null "The full name of the user";
       hashedPassword = mkOpt str null "The hashed password for the user";
-      extraGroups = mkOpt (listOf str) [] "Additional groups for the user";
-      extraOptions = mkOpt attrs {} "Extra options passed to users.users.<name>";
-      sshKeys = mkOpt (listOf str) [] "List of SSH keys for the user";
+      extraGroups = mkOpt (listOf str) [ ] "Additional groups for the user";
+      extraOptions = mkOpt attrs { } "Extra options passed to users.users.<name>";
+      sshKeys = mkOpt (listOf str) [ ] "List of SSH keys for the user";
     };
   };
-in {
+in
+{
   options.aria.users = with types; {
     enable = mkBoolOpt false "Whether to enable user management";
-    users = mkOpt (attrsOf userType) {} "User configurations";
+    users = mkOpt (attrsOf userType) { } "User configurations";
     defaultUserShell = mkOpt package pkgs.zsh "Default shell for users";
 
     defaultGroups = mkOpt (listOf str) [
@@ -36,12 +37,12 @@ in {
       "wheel"
     ] "Groups to add to all normal users";
 
-    extraGroups = mkOpt (listOf str) [] "Additional groups to add to all normal users";
+    extraGroups = mkOpt (listOf str) [ ] "Additional groups to add to all normal users";
 
     root = {
       enable = mkBoolOpt false "Whether to configure root user";
       hashedPassword = mkOpt str null "Root's hashed password";
-      extraGroups = mkOpt (listOf str) [] "Additional groups for root";
+      extraGroups = mkOpt (listOf str) [ ] "Additional groups for root";
     };
   };
 
@@ -49,31 +50,18 @@ in {
     (lib.mkIf cfg.enable {
       users.mutableUsers = false;
 
-      users.users =
-        lib.mapAttrs (
-          name: userCfg:
-            {
-              inherit (userCfg) name hashedPassword;
-              isNormalUser = true;
-              home = "/home/${name}";
-              shell = cfg.defaultUserShell;
-              extraGroups = ifTheyExist (cfg.defaultGroups ++ cfg.extraGroups ++ userCfg.extraGroups);
-              openssh.authorizedKeys.keys = userCfg.sshKeys;
-            }
-            // userCfg.extraOptions
-        )
-        cfg.users;
-
-      # users.users = lib.mkIf cfg.root.enable {
-      #   root = {
-      #     shell = cfg.defaultUserShell;
-      #     extraGroups = ifTheyExist cfg.root.extraGroups;
-      #   } // lib.optionalAttrs (cfg.root.hashedPassword != null) {
-      #     inherit (cfg.root) hashedPassword;
-      #   } // lib.optionalAttrs (cfg.root.sshKeys != []) {
-      #     openssh.authorizedKeys.keys = cfg.root.sshKeys;
-      #   };
-      # };
+      users.users = lib.mapAttrs (
+        name: userCfg:
+        {
+          inherit (userCfg) name hashedPassword;
+          isNormalUser = true;
+          home = "/home/${name}";
+          shell = cfg.defaultUserShell;
+          extraGroups = ifTheyExist (cfg.defaultGroups ++ cfg.extraGroups ++ userCfg.extraGroups);
+          openssh.authorizedKeys.keys = userCfg.sshKeys;
+        }
+        // userCfg.extraOptions
+      ) cfg.users;
     })
   ];
 }
