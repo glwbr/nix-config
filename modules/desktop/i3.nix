b7db9495f1d087d6dd0256721929d967adfec9b6
glwbr <glauber.silva14@gmail.com>
2025-03-11 14:39:57 -0300

    feat(desktop/i3): add custom display configuration

--- modules/desktop/i3.nix
+++ modules/desktop/i3.nix
@@ -3,10 +3,12 @@
   lib,
   pkgs,
   ...
-}: let
+}:
+let
   cfg = config.aria.wms.i3;
   inherit (lib.aria) mkBoolOpt;
-in {
+in
+{
   options.aria.wms.i3 = {
     enable = mkBoolOpt false "Whether to enable i3";
   };
@@ -16,12 +18,33 @@ in {
     services.xserver = {
       enable = true;
       desktopManager.xterm.enable = false;
+      displayManager.sessionCommands = ''
+        ${pkgs.xorg.xset}/bin/xset r rate 170 50
+        ${pkgs.xorg.xrandr}/bin/xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --primary --mode 2560x1440 --pos 1920x0 --rotate normal
+      '';
+
       windowManager.i3 = {
         enable = true;
         package = pkgs.i3-gaps;
       };
+
+      xkb = {
+        model = "pc104";
+        layout = "us,us";
+        options = "compose:ralt, caps:swapescape, grp:alt_space_toggle";
+        variant = ",intl";
+      };
+
+      xrandrHeads = [
+        {
+          output = "HDMI-1";
+          primary = true;
+        }
+        { output = "eDP-1"; }
+      ];
     };
 
-    environment.systemPackages = [pkgs.xclip];
+    environment.pathsToLink = [ "/libexec" ];
+    environment.systemPackages = [ pkgs.xclip ];
   };
 }
