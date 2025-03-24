33fff0c763d4a66f921e9e47706ccf8443c195bc
Glauber S. Santana <hello@glwbr.me>
2025-03-24 13:12:57 -0300

    fix(tmux): re-order plugin loading and its configs

--- modules/tools/tmux/default.nix
+++ modules/tools/tmux/default.nix
@@ -31,6 +31,18 @@ in
         resurrect
         continuum
       ];
+      extraConfigBeforePlugins = ''
+        # Configuration for resurrect plugin
+        set -g @resurrect-strategy-nvim 'session'
+        set -g @resurrect-capture-pane-contents 'on'
+        set -g @resurrect-processes 'ssh btm yazi'
+        set -g @resurrect-dir '~/.local/state/tmux/resurrect'
+
+        # Configuration for continuum plugin
+        set -g @continuum-restore 'on'
+        set -g @continuum-save-interval '15'
+        set -g status-right 'Continuum status: #{continuum_status}'
+      '';
       extraConfig = builtins.readFile ./tmux.conf;
     };
   };
