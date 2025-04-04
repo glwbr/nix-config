64009927f21843bd54382749b4af4bd01ecb2c69
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2025-04-04 19:38:35 -0300

    refactor(profiles/minimal): update defaults

--- modules/profiles/minimal.nix
+++ modules/profiles/minimal.nix
@@ -1,54 +1,55 @@
 {
   config,
   lib,
-  pkgs,
   ...
-}: let
+}:
+let
   inherit (lib.aria) disabled enabled mkBoolOpt;
-
   cfg = config.aria.profiles.minimal;
-in {
+in
+{
   options.aria.profiles.minimal = {
-    enable = mkBoolOpt false "Whether to enable minimal profile";
+    enable = mkBoolOpt true "Wether to apply the minimal configuration";
   };
 
   config = lib.mkIf cfg.enable {
-    environment.defaultPackages = with pkgs; [
-      git
-      neovim
-    ];
+    programs.nano = disabled;
+    documentation = disabled;
+    environment.defaultPackages = [ ];
+
+    zramSwap = {
+      enable = true;
+      swapDevices = 1;
+      algorithm = "zstd";
+    };
+
+    nvim = enabled;
 
     aria = {
       hardware = {
         wireless.iwd = enabled;
+        input = enabled;
       };
 
-      security = {
-        doas = {
-          enable = true;
-          noPass = true;
+      security.pam = enabled;
+
+      services.openssh = enabled;
+
+      terminal = {
+        shell.zsh = enabled;
+
+        tools = {
+          direnv = enabled;
+          git = enabled;
+          tmux = enabled;
         };
-        pam = enabled;
-      };
-
-      services = {
-        openssh = enabled;
-      };
-
-      shell = {
-        zsh = enabled;
       };
 
       system = {
         boot = enabled;
         locale = enabled;
-        nix = {
-          nh = enabled;
-        };
-        xkb = enabled;
+        nix = enabled;
       };
     };
-
-    programs.nano = disabled;
   };
 }
