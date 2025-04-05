e56d4305d0e396e90c67c46f4120a3770a71998a
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2025-04-05 04:28:12 -0300

    refactor(hosts/sinfonia): move hardware settings boot.nix -> hardware.nix

--- hosts/sinfonia/hardware.nix
+++ hosts/sinfonia/hardware.nix
@@ -4,23 +4,45 @@
   pkgs,
   modulesPath,
   ...
-}: {
-  imports = [(modulesPath + "/installer/scan/not-detected.nix")];
+}:
+{
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = ["nvme"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = [];
-  boot.extraModulePackages = [];
+  hardware = {
+    enableRedistributableFirmware = true;
+    deviceTree.name = "rockchip/rk3566-orangepi-3b-v1.1.dtb";
+  };
+
+  boot = {
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+
+    initrd = {
+      systemd = {
+        enable = true;
+        emergencyAccess = true;
+      };
+      supportedFilesystems = [ "ntfs" ];
+      availableKernelModules = [ "nvme" ];
+      kernelModules = [ "phy_rockchip_naneng_combphy" ];
+    };
+
+    kernelModules = [ ];
+    extraModulePackages = [ ];
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
 
   # fileSystems."/" = { device = "/dev/disk/by-uuid/f222513b-ded1-49fa-b591-20ce86a2fe7f"; fsType = "ext4"; };
 
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos";
     fsType = "ext4";
-    options = ["noatime"];
+    options = [ "noatime" ];
   };
 
-  swapDevices = [];
+  swapDevices = [ ];
 
   networking.useDHCP = lib.mkDefault true;
   networking.interfaces.end0.useDHCP = lib.mkDefault true;
