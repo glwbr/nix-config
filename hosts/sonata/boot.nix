5076579d3e8e6ee350f4f05624ac48d92372f9e0
Glauber Santana <glauber.silva14@gmail.com>
2025-04-21 12:01:00 -0300

    style: apply nixfmt

--- hosts/sonata/boot.nix
+++ hosts/sonata/boot.nix
@@ -1,14 +1,15 @@
-{config, ...}: {
+{ config, ... }:
+{
   boot = {
     initrd = {
-      supportedFilesystems = ["nfs"];
-      kernelModules = ["nfs"];
+      supportedFilesystems = [ "nfs" ];
+      kernelModules = [ "nfs" ];
     };
 
-    binfmt.emulatedSystems = ["aarch64-linux"];
+    binfmt.emulatedSystems = [ "aarch64-linux" ];
 
-    kernelModules = ["v4l2loopback"];
-    kernelParams = ["nvidia.NVreg_PreserveVideoMemoryAllocations=1"];
-    extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
+    kernelModules = [ "v4l2loopback" ];
+    kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
+    extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
   };
 }
