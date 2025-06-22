e741e6b499fd0e5b78c1555ce6d57d318bab4c02
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2025-06-22 12:49:33 -0300

    fix(dbus): use broker impl. by default

--- modules/services/dbus.nix
+++ modules/services/dbus.nix
@@ -6,16 +6,14 @@ in
   options.aria.services.dbus = {
     enable = lib.mkEnableOption "dbus";
 
-    implementation = lib.aria.mkOpt (lib.types.enum [ "broker" "dbus" ]) "broker" "DBus implementation to use.";
-    packages = lib.aria.mkListOpt lib.types.package [ pkgs.gcr ] "Additional packages to add to the dbus environment";
-    enableDconf = lib.aria.mkBoolOpt true "Whether to enable dconf alongside dbus";
+    enableDconf = lib.aria.mkBoolOpt false "Whether to enable dconf alongside dbus";
   };
 
   config = lib.mkIf cfg.enable {
     services.dbus = {
       enable = true;
       implementation = "broker";
-      packages = cfg.packages;
+      packages = lib.optional cfg.enableDconf pkgs.gcr_4;
     };
 
     programs.dconf.enable = lib.mkDefault cfg.enableDconf;
