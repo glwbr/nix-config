7637e54a6bad741a6b96f040390e250199a717ae
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2025-07-18 21:30:52 -0300

    fix: sonata's sshKeys

--- hosts/sonata/default.nix
+++ hosts/sonata/default.nix
@@ -4,11 +4,10 @@ let
   wg = {
     port = 51820;
     subnet = "10.5.5.0/24";
-    endpoint = "vpn.phy0.me:${toString wg.port}";
+    endpoint = "wg.phy0.me:${toString wg.port}";
     serverPubKey = "/ko07sTopPxjUJijyKz4dbd5lXGX0qSpQkrvyJB9+QM=";
   };
 in
-
 {
   imports = [ ./boot.nix ./disko.nix ./hardware.nix inputs.hardware.nixosModules.dell-inspiron-7460 ];
 
@@ -19,19 +18,23 @@ in
 
   aria = {
     core.users = {
+      defaultUserShell = pkgs.zsh;
       users.${username} = {
         name = username;
         email = "hi@phy0.com";
         useSOPSPassword = true;
         fullName = "Glauber Santana";
         extraGroups = [ "wheel" "network" ];
-        sshKeys = [ "ssh-ed25521 AAAAC3NzaC1lZDI1NTE5AAAAIOw9mnJmXKHKGvkdlSHJ7dFP2XhlKvQbKogHxwBXFg9o" ];
-        extraOptions.packages = with pkgs; [ alacritty kitty firefox spotify telegram-desktop (discord.override { withVencord = true; }) ];
+        sshKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOw9mnJmXKHKGvkdlSHJ7dFP2XhlKvQbKogHxwBXFg9o" ];
+        extraOptions.packages = with pkgs; [ kitty firefox spotify telegram-desktop (discord.override { withVencord = true; }) ];
       };
     };
 
     core.nix.nh.flakePath = "/home/glwbr/projects/personal/nix-config";
+    core.nix.ld.enable = true;
+    hardware.graphics.enable = true;
 
+    programs.obs.enable = true;
     programs.shell.zoxide.enable = true;
 
     security.gnupg.enable = true;
