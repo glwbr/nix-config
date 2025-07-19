1b4b5b3f5b2856b4550a3d15c62a4e4aaf16aaa8
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2025-07-18 23:07:49 -0300

    fix: deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # credits https://github.com/Misterio77/nix-config
-
 declare -A HOST_PORTS
 HOST_PORTS["sinfonia"]=2222
 
@@ -16,8 +15,15 @@ fi
 for host in ${hosts//,/ }; do
     SSH_PORT=${HOST_PORTS[$host]:-22}
 
-    echo "deploying $host:$SSH_PORT..."
-    export NIX_SSHOPTS="-A -t -p $SSH_PORT"
+    echo "Deploying $host on port $SSH_PORT..."
+    export NIX_SSHOPTS="-A -p $SSH_PORT"
 
-    nixos-rebuild --flake .\#$host switch --target-host $host --use-remote-sudo --use-substitutes $@
+    # TODO: fix this, probably mDns issue
+    nixos-rebuild switch \
+        --flake ".#$host" \
+        --target-host 192.168.0.2 \
+        --sudo \
+        --ask-sudo-password \
+        --use-substitutes \
+        "$@"
 done
