7109806996c7a6638e266406c45de81e49caf7b6
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2025-07-18 21:32:40 -0300

    fix: update sinfonia's schema

--- hosts/sinfonia/default.nix
+++ hosts/sinfonia/default.nix
@@ -1,54 +1,91 @@
 { config, lib, pkgs, inputs, hostName, ... }:
 let
-  username = "glwbr";
+  network = {
+    interface = "end0";
+    ip = {
+      address = "192.168.0.2";
+      prefixLength = 24;
+    };
+    dns = {
+      localResolver = "127.0.0.1";
+    };
+  };
+
   wg = {
     port = 51820;
     subnet = "10.5.5.0/24";
-    endpoint = "vpn.phy0.me:${toString wg.port}";
+    clientIp = "10.5.5.2/32";
+    endpoint = "wg.phy0.me:${toString wg.port}";
     serverPubKey = "/ko07sTopPxjUJijyKz4dbd5lXGX0qSpQkrvyJB9+QM=";
   };
+
+  ports = {
+    web = [ 80 443 ];
+  };
+
+  user = {
+    name = "glwbr";
+    fullName = "Glauber Santana";
+    email = "glauber.silva14@gmail.com";
+    sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOw9mnJmXKHKGvkdlSHJ7dFP2XhlKvQbKogHxwBXFg9o";
+  };
+
+  allTcpPorts = lib.flatten (lib.attrValues ports);
+  allUdpPorts = [ wg.port ];
 in
 {
   imports = [ ./boot.nix ./hardware.nix ];
 
   aria.core.users = {
     defaultUserShell = pkgs.zsh;
-    users.${username} = {
-      name = username;
+    users.${user.name} = {
+      inherit (user) name fullName email;
       extraGroups = [ "wheel" ];
       useSOPSPassword = true;
-      sshKeys = [ "ssh-ed25521 AAAAC3NzaC1lZDI1NTE5AAAAIOw9mnJmXKHKGvkdlSHJ7dFP2XhlKvQbKogHxwBXFg9o" ];
-      fullName = "Glauber Santana";
-      email = "glauber.silva14@gmail.com";
+      sshKeys = [ user.sshKey ];
     };
   };
 
   aria.virtualisation.docker = {
     enable = true;
-    users = [ username ];
+    users = [ user.name ];
   };
 
-  sops.secrets.wireguard = { sopsFile = ./secrets.yaml; neededForUsers = false; };
+  aria.services = {
+    adguard.enable = true;
+    traefik.enable = true;
+  };
+
+  sops.secrets.wireguard = {
+    sopsFile = ./secrets.yaml;
+    neededForUsers = false;
+  };
 
   networking.wireguard.interfaces.wg0 = {
-    ips = [ "10.5.5.2/32" ];
+    ips = [ wg.clientIp ];
     privateKeyFile = config.sops.secrets.wireguard.path;
     listenPort = wg.port;
-
     peers = [{
       name = hostName;
       publicKey = wg.serverPubKey;
       allowedIPs = [ wg.subnet ];
       endpoint = wg.endpoint;
-
       persistentKeepalive = 25;
       dynamicEndpointRefreshSeconds = 300;
     }];
   };
 
-  networking.firewall.allowedTCPPorts = [ 8096 ];
-  networking.firewall.allowedUDPPorts = [ wg.port ];
-  networking.interfaces.end0.ipv4.addresses = [{ address = "192.168.31.160"; prefixLength = 24; }];
+  networking = {
+    inherit hostName;
+    nameservers = [ network.dns.localResolver ];
+
+    interfaces.${network.interface} = { ipv4.addresses = [ network.ip ]; };
+
+    firewall = {
+      allowedTCPPorts = allTcpPorts;
+      allowedUDPPorts = allUdpPorts;
+    };
+  };
 
   system.build.sdImage = pkgs.callPackage "${inputs.nixpkgs}/nixos/lib/make-disk-image.nix" {
     name = "OPi3B-${hostName}-sd-image";
